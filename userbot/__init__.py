89588b6d2613f5e52b8ff9d8e9087b4d5580323f
dvbhatt <53271096+dvbhatt@users.noreply.github.com>
2019-07-26 22:22:24 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -52,7 +52,7 @@ API_KEY = os.environ.get("API_KEY", "777531030")
 
 API_HASH = os.environ.get("API_HASH", "AAHdIKuH0ZG-3Uh5NiCw3KCyU04R8ONo3HY")
 
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
+STRING_SESSION = os.environ.get("STRING_SESSION", )
 
 LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", "0"))
 
